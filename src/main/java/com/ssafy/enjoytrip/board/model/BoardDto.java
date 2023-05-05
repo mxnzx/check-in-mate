504f12a2c227e4d443785a1a88a3ac3583a34a42
mxnzx <i0692631@naver.com>
2023-05-05 23:57:08 +0900

    refactor: dto 롬복 어노테이션 추가

--- src/main/java/com/ssafy/enjoytrip/board/model/BoardDto.java
+++ src/main/java/com/ssafy/enjoytrip/board/model/BoardDto.java
@@ -1,8 +1,13 @@
 package com.ssafy.enjoytrip.board.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class BoardDto {
-
 	private int articleNo;
 	private String userId;
 	private String userName;
@@ -10,69 +15,4 @@ public class BoardDto {
 	private String content;
 	private int hit;
 	private String registerTime;
-
-	public int getArticleNo() {
-		return articleNo;
-	}
-
-	public void setArticleNo(int articleNo) {
-		this.articleNo = articleNo;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}	
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public int getHit() {
-		return hit;
-	}
-
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-
-	public String getRegisterTime() {
-		return registerTime;
-	}
-
-	public void setRegisterTime(String registerTime) {
-		this.registerTime = registerTime;
-	}
-
-	@Override
-	public String toString() {
-		return "BoardDto [articleNo=" + articleNo + ", userId=" + userId + ", userName=" + userName + ", subject="
-				+ subject + ", content=" + content + ", hit=" + hit + ", registerTime=" + registerTime + "]";
-	}
-
-
-
 }
--- src/main/java/com/ssafy/enjoytrip/notice/model/NoticeDto.java
+++ src/main/java/com/ssafy/enjoytrip/notice/model/NoticeDto.java
@@ -1,6 +1,12 @@
 package com.ssafy.enjoytrip.notice.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class NoticeDto {
 
 	private int articleNo;
@@ -11,66 +17,5 @@ public class NoticeDto {
 	private int hit;
 	private String registerTime;
 
-	public int getArticleNo() {
-		return articleNo;
-	}
-
-	public void setArticleNo(int articleNo) {
-		this.articleNo = articleNo;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}	
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public int getHit() {
-		return hit;
-	}
-
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-
-	public String getRegisterTime() {
-		return registerTime;
-	}
-
-	public void setRegisterTime(String registerTime) {
-		this.registerTime = registerTime;
-	}
-
-	@Override
-	public String toString() {
-		return "NoticeDto [articleNo=" + articleNo + ", userId=" + userId + ", subject=" + subject + ", content="
-				+ content + ", hit=" + hit + ", registerTime=" + registerTime + "]";
-	}
 
 }
