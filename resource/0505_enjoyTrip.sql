0283d590d995752785739acd58d3afd4c9c6c31c
mxnzx <i0692631@naver.com>
2023-05-06 22:38:53 +0900

    docs: 클래스 다이어그램, 스키마 파일 수정본 추가

--- resource/0505_enjoyTrip.sql
+++ resource/0505_enjoyTrip.sql
@@ -21,8 +21,6 @@ USE `enjoytrip` ;
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`sido`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`sido` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`sido` (
   `sido_code` INT NOT NULL,
   `sido_name` VARCHAR(30) NULL DEFAULT NULL,
@@ -34,8 +32,6 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`gugun`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`gugun` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`gugun` (
   `gugun_code` INT NOT NULL,
   `gugun_name` VARCHAR(30) NULL DEFAULT NULL,
@@ -52,8 +48,6 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`attraction_info`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`attraction_info` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`attraction_info` (
   `content_id` INT NOT NULL,
   `content_type_id` INT NULL DEFAULT NULL,
@@ -86,8 +80,6 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`attraction_description`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`attraction_description` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`attraction_description` (
   `content_id` INT NOT NULL,
   `homepage` VARCHAR(100) NULL DEFAULT NULL,
@@ -104,8 +96,6 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`member`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`member` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`member` (
   `user_id` VARCHAR(16) NOT NULL,
   `user_name` VARCHAR(20) NOT NULL,
@@ -122,8 +112,6 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`board`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`board` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`board` (
   `article_no` INT NOT NULL AUTO_INCREMENT,
   `board_user_id` VARCHAR(16) NULL DEFAULT NULL,
@@ -137,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `enjoytrip`.`board` (
     FOREIGN KEY (`board_user_id`)
     REFERENCES `enjoytrip`.`member` (`user_id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 2
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -145,128 +133,31 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`board_comment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`board_comment` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`board_comment` (
   `user_id` VARCHAR(16) NOT NULL,
-  `article_no` INT NULL,
-  `comment` VARCHAR(100) NULL,
-  `parent_id` VARCHAR(16) NULL,
+  `article_no` INT NULL DEFAULT NULL,
+  `comment` VARCHAR(100) NULL DEFAULT NULL,
+  `parent_id` VARCHAR(16) NULL DEFAULT NULL,
   `comment_no` INT NOT NULL AUTO_INCREMENT,
-  `register_time` TIMESTAMP NULL,
-  INDEX `comments_to_board_article_no_fk_idx` (`article_no` ASC) INVISIBLE,
+  `register_time` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`comment_no`),
-  CONSTRAINT `comments_to_members_user_id_fk`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `enjoytrip`.`member` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `comments_to_board_article_no_fk_idx` (`article_no` ASC) INVISIBLE,
+  INDEX `comments_to_members_user_id_fk` (`user_id` ASC) VISIBLE,
   CONSTRAINT `comments_to_board_article_no_fk`
     FOREIGN KEY (`article_no`)
-    REFERENCES `enjoytrip`.`board` (`article_no`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `enjoytrip`.`trip_plan_board`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`trip_plan_board` ;
-
-CREATE TABLE IF NOT EXISTS `enjoytrip`.`trip_plan_board` (
-  `trip_plan_id` INT NOT NULL AUTO_INCREMENT,
-  `register_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `trip_plan_title` VARCHAR(30) NOT NULL,
-  `trip_plan_start_date` DATETIME NOT NULL,
-  `trip_plan_end_date` DATETIME NOT NULL,
-  `trip_plan_detail` VARCHAR(100) NOT NULL,
-  `user_id` VARCHAR(16) NULL,
-  PRIMARY KEY (`trip_plan_id`),
-  INDEX `trip_plan_to_members_user_id_fk_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `trip_plan_to_members_user_id_fk`
+    REFERENCES `enjoytrip`.`board` (`article_no`),
+  CONSTRAINT `comments_to_members_user_id_fk`
     FOREIGN KEY (`user_id`)
-    REFERENCES `enjoytrip`.`member` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `enjoytrip`.`photo_spot`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`photo_spot` ;
-
-CREATE TABLE IF NOT EXISTS `enjoytrip`.`photo_spot` (
-  `photo_spot_id` INT NOT NULL AUTO_INCREMENT,
-  `photo_spot_title` VARCHAR(30) NOT NULL,
-  `photo_spot_hit` INT NULL,
-  `photo_spot_addr` VARCHAR(100) NOT NULL,
-  `photo_spot_detail` VARCHAR(100) NULL,
-  `register_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` VARCHAR(16) NULL,
-  `sido_code` INT NULL,
-  `gugun_code` INT NULL,
-  `latitude` DECIMAL(20,17) NULL,
-  `longtitude` DECIMAL(20,17) NULL,
-  `photo_spotcol` VARCHAR(45) NULL,
-  `image_big` VARCHAR(200) NULL,
-  `image_small` VARCHAR(200) NULL,
-  `mlevel` VARCHAR(2) NULL,
-  INDEX `photo_spot_to_gugun_gugun_code_fk_idx` (`gugun_code` ASC) VISIBLE,
-  INDEX `photo_spot_to_sido_sido_code_fk_idx` (`sido_code` ASC) VISIBLE,
-  INDEX `photo_spot_to_members_user_id_fk_idx` (`user_id` ASC) VISIBLE,
-  PRIMARY KEY (`photo_spot_id`),
-  CONSTRAINT `photo_spot_to_members_user_id_fk`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `enjoytrip`.`member` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `photo_spot_to_sido_sido_code_fk`
-    FOREIGN KEY (`sido_code`)
-    REFERENCES `enjoytrip`.`sido` (`sido_code`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `photo_spot_to_gugun_gugun_code_fk`
-    FOREIGN KEY (`gugun_code`)
-    REFERENCES `enjoytrip`.`gugun` (`gugun_code`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `enjoytrip`.`trip_plan_detail`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`trip_plan_detail` ;
-
-CREATE TABLE IF NOT EXISTS `enjoytrip`.`trip_plan_detail` (
-  `trip_plan_id` INT NOT NULL,
-  `trip_no` VARCHAR(45) NOT NULL,
-  `content_id` INT NOT NULL,
-  `comment` VARCHAR(45) NULL,
-  `seq` INT NULL,
-  `trip_plan_start_date` DATETIME NULL,
-  `trip_plan_end_date` DATETIME NULL,
-  PRIMARY KEY (`trip_plan_id`, `trip_no`, `content_id`),
-  CONSTRAINT `trip_plan_detail_to_trip_plan_board_trip_plan_id_fk`
-    FOREIGN KEY (`trip_plan_id`)
-    REFERENCES `enjoytrip`.`trip_plan_board` (`trip_plan_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `trip_plan_detail_to_attraction_info_content_id_fk`
-    FOREIGN KEY (`trip_plan_id`)
-    REFERENCES `enjoytrip`.`attraction_info` (`content_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `enjoytrip`.`member` (`user_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `enjoytrip`.`notice`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `enjoytrip`.`notice` ;
-
 CREATE TABLE IF NOT EXISTS `enjoytrip`.`notice` (
   `article_no` INT NOT NULL AUTO_INCREMENT,
   `notice_user_id` VARCHAR(16) NULL DEFAULT NULL,
@@ -276,13 +167,115 @@ CREATE TABLE IF NOT EXISTS `enjoytrip`.`notice` (
   `register_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`article_no`),
   INDEX `board_to_members_user_id_fk` (`notice_user_id` ASC) VISIBLE,
-  CONSTRAINT `board_to_members_user_id_fk0`
+  CONSTRAINT `notice_to_member_user_id_fk0`
     FOREIGN KEY (`notice_user_id`)
     REFERENCES `enjoytrip`.`member` (`user_id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 2
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `enjoytrip`.`photo_spot`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `enjoytrip`.`photo_spot` (
+  `photo_spot_id` INT NOT NULL AUTO_INCREMENT,
+  `photo_spot_title` VARCHAR(30) NOT NULL,
+  `photo_spot_hit` INT NULL DEFAULT NULL,
+  `photo_spot_addr` VARCHAR(100) NOT NULL,
+  `photo_spot_detail` VARCHAR(100) NULL DEFAULT NULL,
+  `register_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` VARCHAR(16) NULL DEFAULT NULL,
+  `sido_code` INT NULL DEFAULT NULL,
+  `gugun_code` INT NULL DEFAULT NULL,
+  `latitude` DECIMAL(20,17) NULL DEFAULT NULL,
+  `longtitude` DECIMAL(20,17) NULL DEFAULT NULL,
+  `photo_spotcol` VARCHAR(45) NULL DEFAULT NULL,
+  `image_big` VARCHAR(200) NULL DEFAULT NULL,
+  `image_small` VARCHAR(200) NULL DEFAULT NULL,
+  `mlevel` VARCHAR(2) NULL DEFAULT NULL,
+  PRIMARY KEY (`photo_spot_id`),
+  INDEX `photo_spot_to_gugun_gugun_code_fk_idx` (`gugun_code` ASC) VISIBLE,
+  INDEX `photo_spot_to_sido_sido_code_fk_idx` (`sido_code` ASC) VISIBLE,
+  INDEX `photo_spot_to_members_user_id_fk_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `photo_spot_to_gugun_gugun_code_fk`
+    FOREIGN KEY (`gugun_code`)
+    REFERENCES `enjoytrip`.`gugun` (`gugun_code`),
+  CONSTRAINT `photo_spot_to_members_user_id_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `enjoytrip`.`member` (`user_id`),
+  CONSTRAINT `photo_spot_to_sido_sido_code_fk`
+    FOREIGN KEY (`sido_code`)
+    REFERENCES `enjoytrip`.`sido` (`sido_code`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `enjoytrip`.`trip_plan_board`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `enjoytrip`.`trip_plan_board` (
+  `trip_plan_id` INT NOT NULL AUTO_INCREMENT,
+  `register_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `trip_plan_title` VARCHAR(30) NOT NULL,
+  `trip_plan_start_date` DATETIME NOT NULL,
+  `trip_plan_end_date` DATETIME NOT NULL,
+  `trip_plan_detail` VARCHAR(100) NOT NULL,
+  `user_id` VARCHAR(16) NULL DEFAULT NULL,
+  PRIMARY KEY (`trip_plan_id`),
+  INDEX `trip_plan_to_members_user_id_fk_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `trip_plan_to_members_user_id_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `enjoytrip`.`member` (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `enjoytrip`.`trip_plan_detail`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `enjoytrip`.`trip_plan_detail` (
+  `trip_plan_id` INT NOT NULL,
+  `trip_no` VARCHAR(45) NOT NULL,
+  `content_id` INT NOT NULL,
+  `comment` VARCHAR(45) NULL DEFAULT NULL,
+  `seq` INT NULL DEFAULT NULL,
+  `trip_plan_start_date` DATETIME NULL DEFAULT NULL,
+  `trip_plan_end_date` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`trip_plan_id`, `trip_no`, `content_id`),
+  CONSTRAINT `trip_plan_detail_to_attraction_info_content_id_fk`
+    FOREIGN KEY (`trip_plan_id`)
+    REFERENCES `enjoytrip`.`attraction_info` (`content_id`),
+  CONSTRAINT `trip_plan_detail_to_trip_plan_board_trip_plan_id_fk`
+    FOREIGN KEY (`trip_plan_id`)
+    REFERENCES `enjoytrip`.`trip_plan_board` (`trip_plan_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `enjoytrip`.`scrap_photo_spot`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `enjoytrip`.`scrap_photo_spot` (
+  `user_id` VARCHAR(16) NOT NULL,
+  `photo_spot_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `photo_spot_id`),
+  INDEX `22_idx` (`photo_spot_id` ASC) VISIBLE,
+  CONSTRAINT `member_to_scrap_photo_spot_user_id_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `enjoytrip`.`member` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `photo_spot_to_scrap_photo_spot_photo_spot_id_fk`
+    FOREIGN KEY (`photo_spot_id`)
+    REFERENCES `enjoytrip`.`photo_spot` (`photo_spot_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
