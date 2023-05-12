3d9d6915761c817e3bc5705e1409f5c4b0d71b38
mxnzx <i0692631@naver.com>
2023-05-12 17:38:31 +0900

    feat: 지역별 여행지  마커띄우기 성공

--- frontend/src/components/attr/AttractionMap.vue
+++ frontend/src/components/attr/AttractionMap.vue
@@ -79,6 +79,8 @@ export default {
     return {
       map: null,
       areaUrl: "http://127.0.0.1:9018/navigator/searchSido",
+      positions: null,
+      marker: null,
     };
   },
   methods: {
@@ -149,12 +151,12 @@ export default {
   let sidoCode = document.getElementById("search-sido").value;
   let gugunCode = document.getElementById("search-gugun").value;
   let contentTypeId = document.getElementById("search-content-id").value;
-
+  
+  //패치할 url 생성
   if (parseInt(sidoCode)) searchUrl += "?sidoCode=" + sidoCode;
   if (parseInt(gugunCode)) searchUrl += "&gugunCode=" + gugunCode;
   if (parseInt(contentTypeId)) searchUrl += "&contentTypeId=" + contentTypeId;
   
-  console.log(searchUrl);
   fetch(searchUrl)
     .then((response) => response.json())
     .then((data) => {
@@ -169,11 +171,11 @@ export default {
     document.querySelector("table").setAttribute("style", "display: ;");
     let trips = data;
     let tripList = ``;
-    //지역명 저장할 배열
-    //positions = [];
-  
+    this.positions = [];    //지역명 저장할 배열
+
+    //리스트를 나열할 표에 출력할 여행지리스트를 붙인다
     trips.forEach((area) => {
-      console.log(area);
+      console.log(area);  //찍힘
       tripList += `
       <tr onclick="moveCenter(${area.latitude}, ${area.longitude});">
         <td><img src=${area.imgUrl1} width="100px" height="60px"></td>
@@ -181,32 +183,92 @@ export default {
         <td>${area.address1} ${area.address2}</td>
       </tr>
     `;
-    document.getElementById("trip-list").innerHTML = tripList;
-    })
 
-  //   let markerInfo = {
-  //     content: `<div class="container-fluid bg-light">
-  //                 <p>${area.title}</p>
-  //               </div>
-  //               <div class="container-fluid row">
-  //                 <div class="col-3 attraction-first-card" style="background-image: url(${area.imgUrl1}); height: 3rem">
-  //                 </div>
-  //                 <div class="col">
-  //                   <p>${area.address1}</p>
-  //                 </div>
-  //               </div>
-  //               `,
-  //     latlng: new kakao.maps.LatLng(area.latitude, area.longitude),
-  //   };
-  //   positions.push(markerInfo);
-  //   console.log(positions);
-  // });
-  // document.getElementById("trip-list").innerHTML = tripList;
-  // displayMarker();
-
+    //마커 생성 
+    let markerInfo = {
+      content: `<div class="container-fluid bg-light">
+                  <p>${area.title}</p>
+                </div>
+                <div class="container-fluid row">
+                  <div class="col-3 attraction-first-card" style="background-image: url(${area.imgUrl1}); height: 3rem">
+                  </div>
+                  <div class="col">
+                    <p>${area.address1}</p>
+                  </div>
+                </div>
+                `,
+      latlng: new window.kakao.maps.LatLng(area.latitude, area.longitude),
+    };
+    this.positions.push(markerInfo);
+    console.log(this.positions);
+  });
+  document.getElementById("trip-list").innerHTML = tripList;
+  this.displayMarker();
   },
 
-    initMap() {
+  //마커를 지도에 표시하는 카카오지도 api 코드(그대로 가져왔다)
+  displayMarker() {
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+    for (var i = 0; i < this.positions.length; i++) {
+      // 마커 이미지의 이미지 크기 입니다
+      var imageSize = new window.kakao.maps.Size(24, 35);
+
+      // 마커 이미지를 생성합니다
+      var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      // 마커를 생성합니다
+      var marker = new window.kakao.maps.Marker({
+        map: this.map, // 마커를 표시할 지도
+        position: this.positions[i].latlng, // 마커를 표시할 위치
+        title: this.positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image: markerImage, // 마커 이미지
+      });
+
+      // 마커에 표시할 인포윈도우를 생성합니다
+      var infoWindow = new window.kakao.maps.InfoWindow({
+        content: this.positions[i].content,
+      });
+
+      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+      // 이벤트 리스너로는 클로저를 만들어 등록합니다
+      // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+      window.kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(this.map, marker, infoWindow)
+      );
+      window.kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infoWindow)
+      );
+    }
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+    function makeOverListener(map, marker, infoWindow) {
+      return function () {
+        infoWindow.open(map, marker);
+      };
+    }
+
+    // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+    function makeOutListener(infoWindow) {
+      return function () {
+        infoWindow.close();
+      };
+    }
+
+    // 첫번째 검색 정보를 이용하여 지도 중심을 이동 시킵니다
+    console.log(this.positions[0].latlng); //latlng 정보 가져옴
+    this.map.setCenter(this.positions[0].latlng);
+  },
+
+  moveCenter(lat,lng) {
+    this.map.setCenter(new this.kakao.maps.LatLng(lat, lng));
+  },
+
+  //처음 초기화되어있는 지도를 표시한다
+  initMap() {
       const mapContainer = document.getElementById("map"), // 지도를 표시할 div
         mapOption = {
           center: new window.kakao.maps.LatLng(37.500613, 127.036431), // 지도의 중심좌표
@@ -214,7 +276,7 @@ export default {
         };
       this.map = new window.kakao.maps.Map(mapContainer, mapOption);
     },
-  },
+},
 
 
   mounted() {
