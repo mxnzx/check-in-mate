5c57697d255fcf531d3970d5755f34e2c9d3e27d
choijehyuk <cjh970324@naver.com>
2023-05-14 22:58:57 +0900

    feat: 로그인, 마이페이지 구현완료

--- frontend/src/components/user/MyPage.vue
+++ frontend/src/components/user/MyPage.vue
@@ -1,4 +1,82 @@
-<template></template>
+<template>
+  <!--중앙 content Start-->
+  <div class="container" style="text-align: right">
+    <div class="row" style="height: 100px"></div>
+    <div class="row-4 m-1">
+      <div class="row text-center">
+        <div class="col-4"></div>
+        <div class="col-4">
+          <img src="@/assets/images/etc/no-profile.png" alt="profile" style="height: 100px" />
+        </div>
+        <div class="col-4"></div>
+      </div>
+      <div class="row m-3">
+        <div class="col-4">
+          <p style="padding-top: 8px">아이디</p>
+        </div>
+        <div class="col-6">
+          <input
+            class="form-mypage w-75"
+            type="text"
+            placeholder="아이디자리"
+            readonly="readonly"
+            id="id"
+            name="userId"
+          />
+        </div>
+      </div>
+      <div class="row m-3">
+        <div class="col-4">
+          <p style="padding-top: 8px">비밀번호</p>
+        </div>
+        <div class="col-6">
+          <input
+            class="form-mypage w-75"
+            type="password"
+            id="pw"
+            name="userPwd"
+            placeholder="변경하실 비밀번호를 입력하세요"
+          />
+        </div>
+      </div>
+      <div class="row m-3">
+        <div class="col-4">
+          <p style="padding-top: 8px">비밀번호 확인</p>
+        </div>
+        <div class="col-6">
+          <input
+            class="form-mypage w-75"
+            type="password"
+            id="pwCheck"
+            name="pwCheck"
+            placeholder="변경하신 비밀번호를 한번 더 입력하세요"
+          />
+        </div>
+      </div>
+      <div class="row m-3">
+        <div class="col">
+          <button
+            id="myPageUpdate"
+            type="button"
+            class="btn btn-secondary"
+            data-mdb-dismiss="modal"
+          >
+            수정
+          </button>
+        </div>
+        <div class="col" style="text-align: left">
+          <button id="delete-btn" type="button" class="btn btn-danger" data-bs-dismiss="modal">
+            회원탈퇴
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row" style="height: 100px"></div>
+  </div>
+
+  <!--중앙 content End-->
+</template>
 
 <script>
 export default {};
