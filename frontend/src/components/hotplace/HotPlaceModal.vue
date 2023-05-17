6d836d6cbeeef43fd6a4b4f4ebf0e696399e4cdb
choijehyuk <cjh970324@naver.com>
2023-05-17 17:21:34 +0900

    feat: 핫플레이스 자랑하기 프론트 구현

---
+++ frontend/src/components/hotplace/HotPlaceModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <b-modal
+    ref="hotPlace-modal"
+    v-model="showHotPlaceModal"
+    id="signUpModal"
+    style="text-center"
+    hide-footer
+  >
+    <div class="modal-body">
+      <div class="carousel-inner">
+        <!-- 슬라이드 쇼 -->
+        <div class="carousel-item active">
+          <!--가로-->
+          <img
+            class="d-block w-100"
+            src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt="First slide"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>TEST</h5>
+            <p>testtesttest</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100"
+            src="https://images.pexels.com/photos/2355519/pexels-photo-2355519.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            alt="Second slide"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100"
+            src="https://images.pexels.com/photos/2544554/pexels-photo-2544554.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt="Third slide"
+          />
+        </div>
+
+        <!-- / 슬라이드 쇼 끝 -->
+
+        <!-- 왼쪽 오른쪽 화살표 버튼 -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <!-- <span>Previous</span> -->
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <!-- <span>Next</span> -->
+        </a>
+        <!-- / 화살표 버튼 끝 -->
+
+        <!-- 인디케이터 -->
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active"></li>
+          <!--0번부터시작-->
+          <li data-target="#demo" data-slide-to="1"></li>
+          <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+        <!-- 인디케이터 끝 -->
+      </div>
+    </div>
+    <div class="modal-footer" style="justify-content: center">
+      <div class="inline-block">
+        <b-card-text> 제목 </b-card-text>
+      </div>
+      <b-card-text>
+        This card has supporting text below as a natural lead-in to additional
+        content.
+      </b-card-text>
+    </div>
+    <div class="col-auto text-right">
+      <button
+        type="button"
+        id="btn-register"
+        class="btn btn-outline-primary mb-3"
+        @click="registArticle"
+      >
+        수정
+      </button>
+      <button
+        type="reset"
+        class="btn btn-outline-danger mb-3"
+        style="margin-left: 5px"
+        @click="moveList"
+      >
+        삭제
+      </button>
+    </div>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showHotPlaceModal: false,
+    };
+  },
+  methods: {
+    show() {
+      this.showHotPlaceModal = true;
+    },
+    // hideModal() {
+    //   this.$refs["hotPlace-modal"].hide();
+    // },
+  },
+};
+</script>
+
+<style></style>
