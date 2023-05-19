143cfc4b249c5b8e11e5c0c208297e2fd7fcd9d0
mxnzx <i0692631@naver.com>
2023-05-19 15:23:53 +0900

    feat: 나의여행계획 글쓰기페이지 레이아웃 밑 컴포넌트 배치

--- frontend/src/components/plan/PlanWrite.vue
+++ frontend/src/components/plan/PlanWrite.vue
@@ -9,27 +9,90 @@
  -->
 
 <template>
-    <plan-write-by-date></plan-write-by-date>
+  <div class="container">
+    <div class="row justify-content-center">
+      <!-- 여행정보 공유 게시판 글쓰기 제목 끝 -->
+      <div class="col-lg-8 col-md-10 col-sm-12">
+        <input type="hidden" name="action" value="write" />
+        <!-- 제목입력 시작 -->
+        <div class="mb-3">
+          <label for="subject" class="form-label"></label>
+          <div class="plan-input-title-wrapper">
+            <input type="text" class="form-control plan-input-title" id="subject" name="subject" style="border: none"
+              placeholder="[지역명] 제목을 입력하세요" v-model="subject" />
+          </div>
+
+        </div>
+        <!-- 제목입력 끝 -->
+
+        <hr class="my-3" />
+
+        <!-- 내용입력 시작 -->
+        <div>
+        <div class="col-12 d-flex justify-content-center by-day-text mb-2">
+            DAY 1
+          </div>
+          <plan-write-by-date></plan-write-by-date>
+        </div>
+        <div v-for="(date, index) in dates" :key="index">
+          <div class="col-12 d-flex justify-content-center by-day-text mb-2">
+            DAY {{ index+2 }}
+          </div>
+          <plan-write-by-date></plan-write-by-date>
+        </div>
+        <!-- 내용입력 끝 -->
+
+        <!-- 날짜 추가 버튼 -->
+        <div class="d-flex justify-content-center">
+          <b-button size="sm" pill @click="addDate">날짜 추가하기</b-button>
+        </div>
+
+
+        <!-- 글작성, 목록 버튼 시작 -->
+        <div class="col-auto text-right">
+          <b-button type="button" id="btn-register" class="btn mb-3" @click="registArticle" variant="light">
+            글작성
+          </b-button>
+          <b-button type="reset" class="btn mb-3" style="margin-left: 5px" @click="moveList" variant="light">
+            목록
+          </b-button>
+        </div>
+        <!-- 글작성, 목록 버튼 끝 -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import PlanWriteByDate from "./PlanWriteByDate.vue";
 
 export default {
-    name: "PlanWrite",
-    components: {
-        PlanWriteByDate,
-    },
-    data() {
-        return {
-            articleByDate: [],
-        }
+  name: "PlanWrite",
+  components: {
+    PlanWriteByDate,
+  },
+  data() {
+    return {
+      dates: [],  //추가도니 날짜 컴포넌트들을 저장하는 배열
     }
+  }, 
+  methods: {
+    addDate() {
+
+      this.dates.push({});  //새 날짜 컴포넌트를 배열에 추가
+    }
+  }
 
 
 }
 </script>
 
 <style>
+.plan-input-title::placeholder {
+  font-size: x-large;
+}
 
+.plan-input-title-wrapper input {
+  font-size: x-large;
+}
 </style>
--- frontend/src/components/plan/PlanWriteByDate.vue
+++ frontend/src/components/plan/PlanWriteByDate.vue
@@ -1,16 +1,37 @@
 <!-- 
     날짜 하나 추가될때마다 이 컴포넌트를 가져다 붙인다
     row1
-        day _ <- 누를 때마다 카운트 증가
-    row2
         col1 : 원하는 지역명 검색창 + 리스트
         col2 : 지도
         col3 : 추가해놓은 여행지 목록
-    row3
+    row2
         각 날짜별마다 content
 -->
 <template>
-    <div></div>
+  <div>
+    <!-- 원하는 여행지 선택 목록 시작 -->
+    <div class="container">
+      <div class="row">
+        <div class="col-4" style="background-color: antiquewhite;">
+          col1
+        </div>
+        <div class="col-4" style="background-color: antiquewhite;">
+          col2
+        </div>
+        <div class="col-4" style="background-color: antiquewhite;">
+          col3
+        </div>
+      </div>
+    </div>
+    <!-- 원하는 여행지 선택 목록 끝 -->
+
+    <!-- 날짜별 내용 입력 시작 -->
+    <div class="">
+      <textarea class=" row form-control m-2 mb-4" id="content" name="content" rows="7" style="border: none" placeholder="내용을 입력하세요"
+        v-model="content"></textarea>
+    </div>
+    <!-- 날짜별 내용 입력 끝 -->
+  </div>
 </template>
 
 <script>
@@ -20,5 +41,7 @@ export default {
 </script>
 
 <style>
-
+  .by-day-text{
+    font-size:large;
+  }
 </style>
