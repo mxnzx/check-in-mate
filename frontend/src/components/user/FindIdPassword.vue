fdc0b27d5722f8fadf0b955df5d620f65adf00fa
choijehyuk <cjh970324@naver.com>
2023-05-23 21:52:32 +0900

    feat: 비밀번호 찾기 구현완료( 프론트 더해야함)

--- frontend/src/components/user/FindIdPassword.vue
+++ frontend/src/components/user/FindIdPassword.vue
@@ -63,81 +63,87 @@
       <section>
         <div class="form-container">
           <h2>비밀번호 찾기</h2>
-          <form class="form">
-            <div class="form-group">
+          <div class="form-group">
+            <input
+              name="from_id"
+              placeholder="아이디를 입력해주세요"
+              type="text"
+              required
+              v-model="userid"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              name="from_name"
+              placeholder="이름을 입력해주세요"
+              type="text"
+              v-model="username"
+              required
+            />
+          </div>
+          <div class="row" style="justify-content: center">
+            <p>
               <input
-                name="from_id"
-                placeholder="아이디를 입력해주세요"
+                class="form-control"
                 type="text"
+                id="emailid"
+                name="emailid"
+                placeholder="이메일을 입력해주세요."
                 required
-                v-model="userid"
+                v-model="emailid"
               />
+            </p>
+            <div class="col-1">
+              <span
+                class="input-group-text"
+                style="
+                  background-color: white;
+                  border: none;
+                  padding-left: 2.5px;
+                "
+                >@</span
+              >
             </div>
-            <div class="form-group">
-              <input
-                name="from_name"
-                placeholder="이름을 입력해주세요"
-                type="text"
-                v-model="username"
-                required
-              />
+            <div>
+              <b-select
+                class="form-select"
+                id="emaildomain_join"
+                name="emailDomain"
+                v-model="emaildomain"
+              >
+                <option value="ssafy.com">SSAFY</option>
+                <option value="google.com">GOOGLE</option>
+                <option value="kakao.com">KAKAO</option>
+                <option value="naver.com">NAVER</option>
+              </b-select>
             </div>
-            <div class="row" style="justify-content: center">
-              <p>
-                <input
-                  class="form-control"
-                  type="text"
-                  id="emailid"
-                  name="emailid"
-                  placeholder="이메일을 입력해주세요."
-                  required
-                  v-model="emailid"
-                />
-              </p>
-              <div class="col-1">
-                <span
-                  class="input-group-text"
-                  style="
-                    background-color: white;
-                    border: none;
-                    padding-left: 2.5px;
-                  "
-                  >@</span
-                >
-              </div>
-              <div>
-                <b-select
-                  class="form-select"
-                  id="emaildomain_join"
-                  name="emailDomain"
-                  v-model="emaildomain"
-                >
-                  <option value="ssafy.com">SSAFY</option>
-                  <option value="google.com">GOOGLE</option>
-                  <option value="kakao.com">KAKAO</option>
-                  <option value="naver.com">NAVER</option>
-                </b-select>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <input class="submit" type="submit" name="send" />
-            </div>
-          </form>
-        </div></section
-    ></b-tab>
+          </div>
+          <div class="form-group">
+            <button
+              class="btnclass"
+              type="btn"
+              name="send"
+              @click="findPassword"
+            >
+              비밀번호 찾기
+            </button>
+          </div>
+        </div>
+      </section></b-tab
+    >
   </b-tabs>
 </template>
 
 <script>
+import emailjs from "emailjs-com";
 export default {
   data() {
     return {
+      userId: "",
+      userName: "",
       emailId: "",
       emailDomain: "",
-      userid: "",
-      username: "",
-      userpwd: "",
+      userPwd: "",
     };
   },
   methods: {
@@ -168,8 +174,17 @@ export default {
 
     // 비밀번호 찾기
     findPassword() {
+      console.log(
+        this.userid +
+          " " +
+          this.username +
+          " " +
+          this.emailid +
+          "  " +
+          this.emaildomain
+      );
       fetch(
-        `http://localhost:9018/user/find/${this.emailid}/${this.emaildomain}`,
+        `http://localhost:9018/user/find/${this.userid}/${this.username}/${this.emailid}/${this.emaildomain}`,
         {
           method: "GET",
         }
@@ -182,13 +197,39 @@ export default {
           }
         })
         .then((data) => {
-          this.userid = data.findid.userid;
-          console.log(this.userid);
-          //console.log(data);
+          this.userPwd = data.findpassword.userpwd;
+          console.log(this.userPwd);
+          console.log(this.username);
+          this.sendEmail(this.username, this.userPwd);
         })
         .catch((error) => {
           console.error(error);
         });
+    },
+
+    // 이메일 보내기
+    sendEmail(username, userPwd) {
+      const emailParams = {
+        username: username,
+        userPwd: userPwd,
+      };
+      console.log("email username: " + username);
+      console.log("email userPwd: " + userPwd);
+      emailjs
+        .send(
+          "service_gjbj94f",
+          "template_zfsu1rn",
+          emailParams,
+          "4bqA46x4Eeb0JW3IY"
+        )
+        .then(
+          (result) => {
+            console.log("이메일 전송 성공", result.status, result.text);
+          },
+          (error) => {
+            console.log("이메일 전송 실패", error);
+          }
+        );
     },
   },
 };
